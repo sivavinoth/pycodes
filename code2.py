9c5acec30aafac92e2a06e5e69e685226e130550
sivavinoth <jmsivavinoth@example.com>
2024-03-24 11:27:02 +0530

    code changes

--- code2.py
+++ code2.py
@@ -8,4 +8,3 @@ print(type(my_tuples))
 
 
 
-
---
+++ code3.py
@@ -0,0 +1,11 @@
+def find():
+    flag = []
+    smlist = ['a','b','c','a']
+    for dup_count in smlist:  
+        if smlist.count(dup_count)>1:
+            if dup_count not in flag:
+             flag.append(dup_count)
+    print(flag)
+find()
+
+
