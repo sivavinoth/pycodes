72e3ea3bff138fa92840792119c7d3d2c490e05b
sivavinoth <jmsivavinoth@gmail.com>
2024-04-24 18:34:19 +0530

    push

--- python/oop1.py
+++ python/oop1.py
@@ -1,9 +1,14 @@
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
 
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
 
-
-
-
-
+cat = Pets("puppy")
+cat.walk()
 
 
 
--- python/oop2.py
+++ python/oop2.py
@@ -1,15 +1,27 @@
-#Given the below class:
 class Cat:
     species = 'mammal'
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    # 1 Instantiate the Cat object with 3 cats
+
 cat = Cat('tom', 7)
 cat1 = Cat('jerry', 5)
 cat2 = Cat('spike', 12)
 
-# 2 Create a function that finds the oldest cat
 def age(*args):
     return max(*args)
+
 print(f'the max age of cat is{age(cat.age, cat1.age, cat2.age)}')
+
+ 
+
+
+
+
+
+
+
+
+
+
